6e6f28adb71f4083ced2aafa6306f2f4b2447617
Mallikarjun B Y <mallikarjun09051997@gmail.com>
2026-03-10 19:25:58 +0530

    update interview room and email functionality

--- smart_interview_suite/app.py
+++ smart_interview_suite/app.py
@@ -37,7 +37,11 @@ def interview_room(room_id):
    {
    "iceServers":[
    {"urls":["stun:stun.l.google.com:19302"]},
-   {"urls":["stun:stun1.l.google.com:19302"]}
+   {
+            "urls": "turn:openrelay.metered.ca:80",
+            "username": "openrelayproject",
+            "credential": "openrelayproject"
+        }
    ]
    })
 
